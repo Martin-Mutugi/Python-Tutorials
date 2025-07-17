54d859c2aed607bb9bd8c98deef1c15ed67293ad
Martin <mutugimartin967@gmail.com>
2025-07-17 19:31:09 +0300

    I have eddited hello.py file

--- hello.py
+++ hello.py
@@ -1,1 +1,6 @@
-print("hello world")
+a=int(input("enter the length of rectangle :" ))
+b=int(input("enter the breath of the rectangle : "))
+peri=2*(a+b)
+area=a*b
+print("perimeter of rectangle is " , peri)
+print("area of rectangel is " ,area )
